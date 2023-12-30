226f37d30afa670bf62413b1ae982d39fd729431
Renato Rodrigues <renato072002@gmail.com>
2023-12-30 02:44:49 -0300

    changes

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import { MdEmail } from "react-icons/md";
+
 const Contact = () => {
     return (
       <div id="contact" className="mt-[-5%] w-full h-screen bg-[#0a192f] flex justify-center items-center p-4">
@@ -6,44 +9,14 @@ const Contact = () => {
             <p className="text-5xl font-bold inline border-b-4 border-pink-600 text-gray-300">
               Contato
             </p>
-            <p className="text-gray-300 text-[19px] py-4"><span>/ </span>Me mande um e-mail</p>
+            <p className="text-gray-300 text-[21px] py-4 text-center">Me envie um email para conversarmos! Você também pode me encontrar no <a target="_blank" rel="nofollow" href="https://www.linkedin.com/in/renato-rodrigues-2240251b5/" className="underline decoration-sky-600 hover:text-sky-700">Linkedin</a> se desejar mais praticidade!.</p>
+              <div>
+                <a href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=renato072002@gmail.com" className="text-2xl flex items-center justify-center gap-3
+                text-pink-600 hover:text-pink-800">
+                  <MdEmail/>
+                  renato072002@gmail.com</a>
+              </div>
           </div>
-  
-          <input type="hidden" name="accessKey" value={"513d613e-07c2-4e7c-9828-e597b8914680"}></input>
-           <input type="hidden" name="redirectTo" value="http://localhost:3000/"></input>
-  
-          <label htmlFor=""></label>
-          <input
-            className="bg-[#ccd6f6] p-3"
-            type="text"
-            placeholder="Nome"
-            name="nome"
-            required
-          />
-  
-          <input
-            className="my-4 p-3 bg-[#ccd6f6]"
-            type="text"
-            placeholder="Email"
-            name="email"
-            required
-          />
-  
-          <textarea
-            className="bg-[#ccd6f6] p-4"
-            name="message"
-            rows={10}
-            placeholder="Mensagem"
-            required
-          />
-  
-          <button
-            type="submit"
-            className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex item-center"
-          >
-            
-            Enviar
-          </button>
         </form>
       </div>
     );
