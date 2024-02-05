98ccf71dc5ddd4051d7bf1101db6c8a71132e18d
Renato Rodrigues <renato072002@gmail.com>
2024-02-05 03:24:51 -0300

    update

--- app/components/pages/home/known-techs/index.tsx
+++ app/components/pages/home/known-techs/index.tsx
@@ -1,22 +1,21 @@
 import { SectionTitle } from "@/app/components/section-title"
-import { KnownTech } from "./known-tech"
+import { KnownTech  } from "./known-tech"
 import { TbBrandNextjs } from "react-icons/tb"
+import { KnownTech as IKnownTech } from "@/app/types/projects"
 
 
+type KnownTechsProps = {
+    techs: IKnownTech[]
+}
 
-
-export const KnownTechs = () => {
+export const KnownTechs = ({ techs }: KnownTechsProps) => {
     return (
         <section className="container py-16">
             <SectionTitle subtile="Competências" title="Conhecimentos" />
 
             <div className="grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3 mt-[60px]">
-                {Array.from({ length: 8 }).map((_, index) => (
-                    <KnownTech tech={{
-                        icon: <TbBrandNextjs />,
-                        name: "Next.js",
-                        startDate: "2024-02-01"
-                    }} />
+                {techs?.map((tech) =>(
+                    <KnownTech key={tech.name} tech={tech} />
                 ))}
             </div>
         </section>
