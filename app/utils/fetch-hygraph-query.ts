d193b5f9843fec42cd32df833bc7de288ec17fae
Renato Rodrigues <renato072002@gmail.com>
2024-02-05 02:41:51 -0300

    update

--- app/utils/fetch-hygraph-query.ts
+++ app/utils/fetch-hygraph-query.ts
@@ -1,7 +1,4 @@
-export const fetchHygraphQuery = async <T>(
-    query: string,
-    revalidate?: number,
-): Promise<T> => {
+export const fetchHygraphQuery = async(query: string) => {
     const response = await fetch(process.env.HYGRAPH_URL!, {
         method: 'POST',
         headers: {
@@ -9,10 +6,10 @@ export const fetchHygraphQuery = async <T>(
             Accept: 'application/json',
             Authorization: `Bearer ${process.env.HYGRAPH_TOKEN}`,
         },
-        next: {
-            revalidate,
-        },
         body: JSON.stringify({ query }),
+        next: {
+            revalidate: 60 * 60 * 24
+        }
     })
 
     const { data } = await response.json()
