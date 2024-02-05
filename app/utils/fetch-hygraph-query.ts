3c3fd9e355e35ebeef4759ab2fab4a1db4e75ee8
Renato Rodrigues <renato072002@gmail.com>
2024-02-05 19:35:07 -0300

    Update fetch-hygraph-query.ts

--- app/utils/fetch-hygraph-query.ts
+++ app/utils/fetch-hygraph-query.ts
@@ -1,24 +1,23 @@
-export const fetchHygraphQuery = async (query: string) => {
-    try {
-        const response = await fetch(process.env.HYGRAPH_URL!, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'Authorization': `Bearer ${process.env.HYGRAPH_TOKEN}`
-            },
-            body: JSON.stringify({ query })
-        });
-
-        if (!response.ok) {
-            throw new Error(`Erro ao realizar a solicitação: ${response.statusText}`);
-        }
-
-        const { data } = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Erro ao executar a consulta GraphQL:', error);
-        throw error; // Propague o erro para o código que chama fetchHygraphQuery
-    }
-}
-
+export const fetchHygraphQuery = async <T>(
+    query: string,
+    revalidate?: number,
+  ): Promise<T> => {
+    const response = await fetch(process.env.HYGRAPH_URL!, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: `Bearer ${process.env.HYGRAPH_TOKEN}`,
+      },
+      next: {
+        revalidate,
+      },
+      body: JSON.stringify({
+        query,
+      }),
+    })
+  
+    const { data } = await response.json()
+  
+    return data
+  }
