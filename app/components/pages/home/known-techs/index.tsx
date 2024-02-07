75031df7ab1dedede87dc539ca0009d95c6c6229
Renato Rodrigues <renato072002@gmail.com>
2024-02-06 22:51:16 -0300

    Update index.tsx

--- app/components/pages/home/known-techs/index.tsx
+++ app/components/pages/home/known-techs/index.tsx
@@ -21,7 +21,7 @@ export const KnownTechs = ({ techs }: KnownTechsProps) => {
                     <motion.div
                         key={tech.name}
                         initial={{ opacity: 0, x: -100 }}
-                        animate={{ opacity: 1, x: 0 }}
+                        whileInView={{ opacity: 1, x: 0 }}
                         exit={{ opacity: 0, x: -100 }}
                         transition={{ duration: 0.5, delay: i * 0.1 }}
 
