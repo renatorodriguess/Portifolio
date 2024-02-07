52a3b6f6ac230223866a168ae8898b0873027824
Renato Rodrigues <renato072002@gmail.com>
2024-02-06 23:05:38 -0300

    animation.framer3

--- app/components/pages/projects/projects-list/index.tsx
+++ app/components/pages/projects/projects-list/index.tsx
@@ -1,6 +1,10 @@
+'use client'
+
 import Link from "next/link"
 import { ProjectCard } from "./project-card"
 import { Project } from "@/app/types/projects"
+import { motion } from "framer-motion"
+import { fadeUpAnimation } from "@/app/lib/animations"
 
 
 type ProjectListProps = {
@@ -11,10 +15,16 @@ type ProjectListProps = {
 export const ProjectList = ({ projects }: ProjectListProps) => {
     return (
         <section className="container py-32 grid grid-cols-1 sm:grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-x-4 gap-y-6">
-            {projects.map(project => (
-                <Link href="/projects/space-y">
-                    <ProjectCard project={project} />
-                </Link>
+            {projects.map((project, i) => (
+                <motion.div 
+                key={project.title}
+                {...fadeUpAnimation}
+                transition={{ duration: 0.5, delay: i * 0.1 }}
+                >
+                    <Link href="/projects/space-y">
+                        <ProjectCard project={project} />
+                    </Link>
+                </motion.div>
             ))}
         </section>
     )
