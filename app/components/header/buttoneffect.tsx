11bd149253b5391a7af9ec001fe814b658afdd53
Renato Rodrigues <renato072002@gmail.com>
2024-02-13 22:42:06 -0300

    Update buttoneffect.tsx

--- app/components/header/buttoneffect.tsx
+++ app/components/header/buttoneffect.tsx
@@ -7,7 +7,7 @@ export const ButtonEffect = () => {
             <span className="absolute right-0 flex items-center justify-start w-8 sm:w-10 h-8 sm:h-10 duration-300 transform translate-x-full group-hover:translate-x-2 ease">
                 <svg className="w-4 sm:w-5 h-4 sm:h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
             </span>
-            <span className="relative">Download CV</span>
+            <span className="relative">Currículo</span>
         </a>
     )
 }
